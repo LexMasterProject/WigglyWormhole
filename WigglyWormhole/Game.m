a54ea973235a25e1ae9384c1e5bc37ec4585e9e6
AlexWei <wwei13@sheffield.ac.uk>
2014-10-29 21:38:33 +0000

    refactory

--- WigglyWormhole/Game.m
+++ WigglyWormhole/Game.m
@@ -16,6 +16,7 @@
 @implementation Game
 {
     NSMutableArray*_wormArr;
+    NSMutableArray*_emptyCell;
     
 }
 
@@ -26,7 +27,7 @@
         _title=GAME_TITLE;
         _score=0;
         _wormDirection=DOWN;
-        [self initMap];
+        
         
          NSMutableArray*head=[NSMutableArray arrayWithObjects:[NSNumber numberWithInt:10],
              [NSNumber numberWithInt:7],
@@ -35,6 +36,16 @@
         _wormArr=[NSMutableArray arrayWithObjects:
                   head,
                   nil];
+        
+        
+        _emptyCell=[NSMutableArray arrayWithCapacity:SCENE_CELLS_X*SCENE_CELLS_Y];
+        for (int i=0; i<SCENE_CELLS_Y*SCENE_CELLS_X; i++) {
+            [_emptyCell addObject:[NSNumber numberWithInt:i]];
+        }
+       [self initMap];
+        
+      
+        
         _isWormDead=0;
         
         
@@ -55,27 +66,32 @@
         }
     }
     
+     [_emptyCell removeObjectAtIndex:10*14+7];
+      _map[10][7]=[NSNumber numberWithInt:WORM_FACE_INDEX];
     
-    for (int i=0; i<WORM_HOLE_NUMBER;) {
-        int x=RAND_FROM_TO(0, 19);
-        int y=RAND_FROM_TO(0, 13);
-        if ([_map[x][y] integerValue]==GRASS_LAND_INDEX) {
-            _map[x][y]=[NSNumber numberWithInt:WORM_HOLE_INDEX];
-            i++;
-        }
+    for (int i=0; i<WORM_HOLE_NUMBER;i++) {
+        int index=RAND_FROM_TO(0, [_emptyCell count]-1);
+        int posInfo=[_emptyCell[index] integerValue];
+        int x=posInfo/14;
+        int y=posInfo%14;
+        
+        [_emptyCell removeObjectAtIndex:index];
+         _map[x][y]=[NSNumber numberWithInt:WORM_HOLE_INDEX];
+        
     }
+
+    for (int i=0; i<MUSHROOM_NUMBER;i++ ) {
+        int index=RAND_FROM_TO(0, [_emptyCell count]-1);
+        int posInfo=[_emptyCell[index] integerValue];
+        int x=posInfo/14;
+        int y=posInfo%14;
     
-    for (int i=0; i<MUSHROOM_NUMBER; ) {
-        int x=RAND_FROM_TO(0, 19);
-        int y=RAND_FROM_TO(0, 13);
-        if ([_map[x][y] integerValue]==GRASS_LAND_INDEX) {
-            _map[x][y]=[NSNumber numberWithInt:MUSHROOM_INDEX];
-            i++;
-        }
+        [_emptyCell removeObjectAtIndex:index];
+        _map[x][y]=[NSNumber numberWithInt:MUSHROOM_INDEX];
 
     }
     
-    _map[10][7]=[NSNumber numberWithInt:WORM_FACE_INDEX];
+    
     
 }
 -(void)updateMapAndWormWithNextX:(int)nextx andY:(int)nexty
@@ -84,34 +100,46 @@
     int heady=[_wormArr[0][1] integerValue];
     int tailx=[[_wormArr lastObject][0] integerValue];
     int taily=[[_wormArr lastObject][1] integerValue];
-    if ([_map[nextx][nexty]integerValue]==MUSHROOM_INDEX) {
+    
+    switch ([_map[nextx][nexty]integerValue]) {
+        case MUSHROOM_INDEX:
+            //update map
+            _map[nextx][nexty]=[NSNumber numberWithInt:WORM_FACE_INDEX];
+            _map[headx][heady]=[NSNumber numberWithInt:WORM_BODY_INDEX];
+            //add new head
+            [_wormArr insertObject:[NSMutableArray arrayWithObjects:
+                                    [NSNumber numberWithInt:nextx],
+                                    [NSNumber numberWithInt:nexty],
+                                    nil] atIndex:0];
+            //leave tail there
+            break;
+        case GRASS_LAND_INDEX:
         
-        //update map
-        _map[nextx][nexty]=[NSNumber numberWithInt:WORM_FACE_INDEX];
-        _map[headx][heady]=[NSNumber numberWithInt:WORM_BODY_INDEX];
-        //add new head
-        [_wormArr insertObject:[NSMutableArray arrayWithObjects:
-                                [NSNumber numberWithInt:nextx],
-                                [NSNumber numberWithInt:nexty],
-                                nil] atIndex:0];
-        //leave tail there
-        
-    }
-    else
-    {
-        //update map
-        _map[nextx][nexty]=[NSNumber numberWithInt:WORM_FACE_INDEX];
-        _map[headx][heady]=[NSNumber numberWithInt:WORM_BODY_INDEX];
-        _map[tailx][taily]=[NSNumber numberWithInt:GRASS_LAND_INDEX];
-        //add new head
-        [_wormArr insertObject:[NSMutableArray arrayWithObjects:
-                                [NSNumber numberWithInt:nextx],
-                                [NSNumber numberWithInt:nexty],
-                                nil] atIndex:0];
-        
-        //cut tail
-        [_wormArr removeLastObject];
-        
+            //update map
+            for (int i=0; i<[_emptyCell count]; i++) {
+                if (nextx*14+taily==[_emptyCell[i] integerValue]) {
+                    [_emptyCell removeObjectAtIndex:i];
+                    break;
+                }
+            }
+            _map[nextx][nexty]=[NSNumber numberWithInt:WORM_FACE_INDEX];
+            _map[headx][heady]=[NSNumber numberWithInt:WORM_BODY_INDEX];
+            _map[tailx][taily]=[NSNumber numberWithInt:GRASS_LAND_INDEX];
+            [_emptyCell addObject:[NSNumber numberWithInt:14*tailx+taily]];
+            //add new head
+            [_wormArr insertObject:[NSMutableArray arrayWithObjects:
+                                    [NSNumber numberWithInt:nextx],
+                                    [NSNumber numberWithInt:nexty],
+                                    nil] atIndex:0];
+            
+            //cut tail
+            [_wormArr removeLastObject];
+            break;
+        case WORM_HOLE_INDEX:
+        case WORM_BODY_INDEX:
+            _wormDirection=DIRECTION_NONE;
+        default:
+            break;
     }
 
 }
