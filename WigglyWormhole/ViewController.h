d9885613a923b0321878e561b6855b293c4e3a4c
AlexWei <wwei13@sheffield.ac.uk>
2014-11-04 01:18:23 +0000

    update

--- WigglyWormhole/ViewController.h
+++ WigglyWormhole/ViewController.h
@@ -15,6 +15,7 @@
 @property (strong)Game*game;
 @property (strong) GameScene*gameScene;
 @property (strong) NSTimer*nsTimer;
+@property (strong) NSTimer*nsCountDown;
 
 
 @property (weak, nonatomic) IBOutlet UILabel *uiGameTitle;
